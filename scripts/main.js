93463c5f0a1376608761a3f87bfd9a2cd979c097
miirsery <sania.nika@mail.ru>
2021-09-20 20:24:07 +0700

    add blog and fixed something

--- scripts/main.js
+++ scripts/main.js
@@ -21,6 +21,12 @@ function burgerMenu(selector) {
     }
   }
 }
-
 burgerMenu('.burger-menu')
 
+const accordion = document.getElementsByClassName('contentBx')
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+  })
+}
